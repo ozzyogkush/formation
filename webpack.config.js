5a2b1016b1e3e70bca71983143061c8db9b2be67
Derek Rosenzweig <derek.rosenzweig@gmail.com>
2016-07-10 22:08:18 -0400

    Add dependencies for babel, expose, jquery, stampit, and webpack. Add simple webpack configuration. Add babelrc with es2015 preset. Update gitignore and README.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// We specify the exposed modules/objects/classes here as it is the WebPacker's
+// concern as to what needs to be exposed to the browser, NOT the module itself's job.
+module.exports = {
+  entry: './src/formation.js',
+  output: {
+    path: './dist',
+    filename: 'formation.js'
+  },
+  module : {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: require.resolve('jquery'), loader : 'expose?$!expose?jQuery' }
+    ]
+  }
+};
